780ca1ea1f9eb9c9ed422ac1286f83be48cd22dd
Pshenychnykh-Dmytro-Madison <dmitriy.p@langate.com>
2023-09-12 13:41:41 +0300

    discard-changes

--- src/app/features/home/guards-demo/guards-demo.component.ts
+++ src/app/features/home/guards-demo/guards-demo.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { GuardsDemoModel } from './guards-demo.model';
+import { ITouchable } from '@core/interfaces/touchable';
 
 @Component({
   selector: 'app-guards-demo',
   templateUrl: './guards-demo.component.html',
   styleUrls: ['./guards-demo.component.scss']
 })
-export class GuardsDemoComponent implements OnInit {
+export class GuardsDemoComponent implements OnInit, ITouchable {
+  private _touched = false;
+
   public model: GuardsDemoModel = {
     address: '',
     email: '',
@@ -17,7 +20,20 @@ export class GuardsDemoComponent implements OnInit {
 
   constructor(private dataSvc: DataService) {}
 
+
+  get isTouched(): boolean {
+    return this._touched;
+  }
+
+  get discardChangesMessage(): string {
+    return null;
+  }
+
   ngOnInit(): void {
     this.dataSvc.getGuardDemo().subscribe(model => this.model = model);
+  }
+
+  public onChange(e: any): void {
+    this._touched = true;
   }
 }
